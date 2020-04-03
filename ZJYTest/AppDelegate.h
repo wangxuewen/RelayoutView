235abba68bd5ad613490b54ce8484290079d79fe
wb-wxw670241 <wb-wxw670241@alibaba-inc.com>
2020-04-03 14:11:12 +0800

    提交单个文件

--- ZJYTest/AppDelegate.h
+++ ZJYTest/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by 王学文 on 2018/1/17.
 //  Copyright © 2018年 王学文. All rights reserved.
 //
-
+// 测试git 回滚问题哈哈哈哈
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
